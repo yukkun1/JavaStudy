d45d140ea3d2fea7a888a74acf59c2d1dd6aff8b
yukkun1 <110563216+yukkun1@users.noreply.github.com>
2022-11-21 21:52:25 +0900

    summary

---
+++ begineer.java
@@ -0,0 +1,46 @@
+public class begineer {
+    public static void main(String[] args) {
+        int num = 3;
+        double pie = 3.14;
+        char blood = 'A';
+        boolean fun = true;
+        String msg = "入門";
+        System.out.println(num);
+        System.out.println(pie);
+        System.out.println(blood);
+        System.out.println(fun);
+        System.out.println(msg);
+
+        String[] name = new String[5];
+        name[0] = "岡本";
+        name[1] = "佐藤";
+        name[2] = "村上";
+        name[3] = "大谷";
+        name[4] = "落合";
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+        System.out.println(name[3]);
+        System.out.println(name[4]);
+
+         int[] data =  new int[4];
+        data[0] = 90;
+        data[1] = 80;
+        data[2] = 100;
+        data[3] = 90;
+        System.out.println("1人目は" + data[0] + "点");
+        System.out.println("2人目は" + data[1] + "点");
+        System.out.println("3人目は" + data[2] + "点");
+        System.out.println("4人目は" + data[3] + "点");
+    
+        /*演習問題2 */
+        int height = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]); 
+        double area; 
+        area = height * width / 2.0;
+        System.out.println("高さ" + height);
+        System.out.println("幅" + width);
+        System.out.println("面積" + area);
+        
+    }
+}
--- example.java
+++ example.java
@@ -1,6 +1,87 @@
 public class example {
-
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-       System.out.println("Hello World");
-    } 
- }
+        int seisu = 1234;
+        System.out.println("int型 = 整数 =" + seisu); 
+        
+        String moji = "javaはプログラム";
+        System.out.println(moji);
+
+        int i =10;
+        System.out.println(i >= 5 && i <= 20);
+
+
+        int old =19;
+        String notAllowed = "未成年";
+        String willBeAllowed = "ギリ未成年";
+        String allowed = "成年";
+        if (old > 20) {
+            System.out.println(allowed);
+        } else if (old == 19) {
+            System.out.println(willBeAllowed);
+        } else  {
+            System.out.println(notAllowed);
+        }
+
+        int age = 56;
+        boolean is_student =true;
+        String portable = "５０代";
+        if (age >= 50 && is_student) {
+            System.out.println(portable);
+        }
+        
+        int kubun = 3;
+        switch (kubun) {
+            case 1:
+            System.out.println("kubunが1の時");
+                break;
+            case 2:
+            System.out.println("kubunが2の時");
+                break;
+            case 3:
+            System.out.println("kubunが3の時");
+                 break;
+        }
+     /*練習問題 */
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        /*hogeDoubleという変数にdoubleで小数点を挿入している */
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        // System.out.printlnでhogeStringの変数の中身を出力
+        System.out.println(hogeString);
+        double hogeDouble = 0.2;
+        System.out.println(hogeDouble);
+
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
+        System.out.println(country.length);
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+        System.out.println(strArray[1]); 
+        //  int型で変数intArrayを作り、5つの数字を格納
+        int[] intArray = {10, 20, 30, 40, 50};
+        //  20と50を足した70を出力している   
+        System.out.println(intArray[1] + intArray[4]);
+ 
+        int plus = 10;
+        plus += 5;
+        int minus = 20;
+        minus -= 7;
+        // int型にmultiplyの変数を作り、10×2=20を算出
+        int multiply = 10 * 2;
+        int remi = 20 % 6;
+        // String型でtownに有楽町、lineの変数に線を格納
+        String town = "有楽町";
+        String line = "線";
+        String train = town + line; 
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi); 
+        System.out.println(train);
+       
+
+    }   
+}   
